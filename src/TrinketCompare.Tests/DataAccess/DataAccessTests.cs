6866617fb08d37e3d42eef52a0fcbe69f35d21fa
Unknown <bitterawakening@googlemail.com>
2017-01-19 03:15:12 +0100

    Basic solution setup

---
+++ src/TrinketCompare.Tests/DataAccess/DataAccessTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using NUnit.Framework;
+using TrinketCompare.Core.DataAccess;
+
+namespace TrinketCompare.Tests
+{
+    [TestFixture]
+    public class DataAccessTests
+    {
+        [Test]
+        public void Existing_Trinket_List_CanBeRead()
+        {
+            var _reader = new DataReader();
+            Assert.NotNull(_reader);
+        }
+    }
+}
